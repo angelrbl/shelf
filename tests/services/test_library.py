05084e4dde7989624a6302dee9716df938a8c4e8
angelrb <angelramibla@gmail.com>
2026-08-06 08:54:30 +0200

    Add tests for updating/removing books
    Add three unit tests to services/test_library.py: test_add_existing_book_updates_state, test_update_book_state, and test_remove_book. These cover behavior for adding an already-present book (state updates), explicitly updating a book's state, and removing a book from a user's shelf. Removes the previous TODO placeholder.

--- tests/services/test_library.py
+++ tests/services/test_library.py
@@ -49,4 +49,44 @@ def test_add_new_book(db_session):
     assert user_shelf[0].book.title == "1984"
     assert user_shelf[0].state == BookState.READ
 
-#TODO: add more tests
+def test_add_existing_book_updates_state(db_session):
+    user = User(username="thebooksreadme", password="ialsolovelee")
+    db_session.add(user)
+    db_session.commit()
+
+    library.add_book(user_id=user.id, book_data=BOOK_DATA, state=BookState.WISHED)
+    library.add_book(user_id=user.id, book_data=BOOK_DATA, state=BookState.READ)
+
+    user_shelf = library.get_user_shelf(user_id=user.id)
+    assert len(user_shelf) == 1
+    assert user_shelf[0].state == BookState.READ
+
+def test_update_book_state(db_session):
+    user = User(username="imaworm", password="leeisthebest")
+    db_session.add(user)
+    db_session.commit()
+
+    library.add_book(user_id=user.id, book_data=BOOK_DATA, state=BookState.WISHED)
+
+    user_shelf = library.get_user_shelf(user_id=user.id)
+    book_id = user_shelf[0].book_id
+
+    library.update_book_state(user_id=user.id, book_id=book_id, new_state=BookState.READ)
+
+    user_shelf = library.get_user_shelf(user_id=user.id)
+    assert user_shelf[0].state == BookState.READ
+
+def test_remove_book(db_session):
+    user = User(username="wormandwarm", password="wassuplee")
+    db_session.add(user)
+    db_session.commit()
+
+    library.add_book(user_id=user.id, book_data=BOOK_DATA, state=BookState.WISHED)
+
+    user_shelf = library.get_user_shelf(user_id=user.id)
+    book_id = user_shelf[0].book_id
+
+    library.remove_book(user_id=user.id, book_id=book_id)
+
+    user_shelf = library.get_user_shelf(user_id=user.id)
+    assert len(user_shelf) == 0
